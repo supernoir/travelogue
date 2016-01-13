9c0659e106b93e13352671027192817f2c9950d4
supernoir <kawohl.bentodesign@gmail.com>
2016-01-13 23:04:43 +0100

    adds milestones

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,10 @@
 angular.module("travelogue", []).controller('mainCtrl', function($scope){
 
-
 $scope.locations = ["London","Bombay","Pune", "Mars"];
-
+    
+$scope.milestones = [
+        {date:'31 July 1980', location:'London, England', event: 'Birthday', cast: 'Harry, Hermione'},
+        {date:'22 October 1979', location:'Pune, India', event: 'Trip to India', cast: 'Hermione'},
+        ];
+    
 });
