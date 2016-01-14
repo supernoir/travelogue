a49a83e2dd2625b664408b227ca46abc5a30289f
supernoir <kawohl.bentodesign@gmail.com>
2016-01-14 03:01:15 +0100

    pushes new milestone to milestone hash in array

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,24 @@ $scope.locations = ["London","Bombay","Pune", "Mars"];
     
 $scope.milestones = [
         {date:'31 July 1980', location:'London, England', event: 'Birthday', cast: 'Harry, Hermione'},
-        {date:'22 October 1979', location:'Pune, India', event: 'Trip to India', cast: 'Hermione'},
+        {date:'22 October 1979', location:'Pune, India', event: 'Trip to India', cast: 'Hermione'}
         ];
 
 $scope.journeynames = []
 
-$scope.addMilestone = function (milestone) {
-    $scope.milestones.push(milestone);
+$scope.createMilestone = function(date,location,event,cast) {
+    
+    var newMilestone = new function() {
+        this.date = $scope.newdate;
+        this.location = $scope.newlocation;
+        this.event = $scope.newevent;
+        this.cast = $scope.newcast;
+        }
+
+
+        $scope.milestones = $scope.milestones.concat(newMilestone);
+        console.log(newMilestone);
+        
     }
 
 $scope.createJourney = function(journeyname) {
