b6d7541cd5cd2084ca376363876c98a6ebe17dea
supernoir <kawohl.bentodesign@gmail.com>
2016-01-16 12:30:59 +0100

    adds createJourney function including UUID generator

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,22 +18,37 @@ $scope.journeys = [
     ];
 
 $scope.createMilestone = function(date,location,event,cast) {
-    
     var newMilestone = new function() {
         this.date = $scope.newdate;
         this.location = $scope.newlocation;
         this.event = $scope.newevent;
         this.cast = $scope.newcast;
         }
-
-
         $scope.milestones = $scope.milestones.concat(newMilestone);
-        console.log(newMilestone);
-        
     }
 
-$scope.createJourney = function(journeyname) {
-    console.log(journeyname);
+$scope.createJourney = function(name,cast,uuid) {
+    var newJourney = new function() {
+        this.name = $scope.journeynewname;
+        this.cast = $scope.journeynewcast;
+        this.uuid = $scope.generateUUID();
+        }
+        $scope.journeys = $scope.journeys.concat(newJourney);
+}
+
+
+// Generate UUID
+$scope.generateUUID = function(){
+    var d = new Date().getTime();
+    if(window.performance && typeof window.performance.now === "function"){
+        d += performance.now();; //use high-precision timer if available
+    }
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
     
 });
