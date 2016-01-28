95c6e9381eb45beebb4919d73aa17691b4ecb4e2
supernoir <kawohl.bentodesign@gmail.com>
2016-01-28 11:14:18 +0100

    includes bodyparser

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 var express  = require('express');
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');   
 var app      = express();                               
 
-var mongoose = require('mongoose');                     
+  
+               
 
 
 // configuration
@@ -10,6 +13,7 @@ mongoose.connect('mongodb://localhost/library');
 
 app.use(express.static(__dirname + '/app'));                 // set the static files location /public/img will be /img for users
 //app.use(morgan('dev'));                                         // log every request to the console
+app.use(express.bodyParser());
 //app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 //app.use(bodyParser.json());                                     // parse application/json
 //app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
@@ -35,13 +39,9 @@ app.get('/characters', function(request, response) {
     });
 
 app.post('/characters', function(request, response){
-    // get data from function
-    // post to api
-    
-    // receive response
-    // or handle error
-    
-    // show tick
+      console.log(request.body);      // your JSON
+  response.send(request.body);    // echo the result back
+
 });
 
 app.get('*', function(request, response) {
