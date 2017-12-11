8834e96071927dfbb6f8e096e79d0fc86ae5ea2c
Conny Kawohl <conny.kawohl@useeds.de>
2017-12-11 16:30:48 +0100

    refactors journeys api

--- server/index.js
+++ server/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const connect = require('connect');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require('express')
+const connect = require('connect')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 
-mongoose.connect('mongodb://localhost/library');
-app.use(express.static(__dirname + '/app'));
+mongoose.connect('mongodb://localhost/library')
+app.use(express.static(__dirname + '/app'))
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
+app.use(morgan('combined'))
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-);
+)
 
 app.use(function(request, response, next) {
-  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Origin', '*')
   response.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
-  next();
-});
+  )
+  response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS')
+  next()
+})
 
 const Characters = mongoose.model('Characters', {
   text: String
-});
+})
 
 const Journeys = mongoose.model('Journeys', {
   name: String,
   cast: Array,
   desc: String
-});
+})
 
 const Milestones = mongoose.model('Journey', {
   journey: String,
@@ -44,44 +44,58 @@ const Milestones = mongoose.model('Journey', {
   date: Date,
   location: String,
   cast: []
-});
+})
 
 app.get('/characters', (request, response) => {
   Characters.find((error, characters) => {
-    if (error) response.send(error);
-    response.json(characters);
-  });
-});
+    if (error) response.send(error)
+    response.json(characters)
+  })
+})
 
-app.get('/journeys', (request, response) => {
+app.get('/journeys/all', (request, response) => {
   Journeys.find((error, journeys) => {
-    if (error) response.send(error);
-    response.json(journeys);
-  });
-});
+    if (error) response.send(error)
+    response.json(journeys)
+  })
+})
 
-app.post('/journeys', (request, response, next) => {
-  const journey = new Journeys();
-  journey.name = request.body.name;
-  journey.cast = request.body.cast;
-  journey.desc = request.body.description;
+app.get('/journeys/:name', function(req, res) {
+  Journeys.findOne({ name: req.params.name }, function(err, journey) {
+    res.json({ journey })
+  })
+})
 
-  journey.save((error, journey) => {
+app.post('/journeys', function(req, res) {
+  const journeyName = req.param('name')
+  const journeyCast = req.param('cast')
+  const journeyDesc = req.param('desc')
+
+  const journey = new Journeys()
+  journey.name = journeyName
+  journey.cast = journeyCast
+  journey.desc = journeyDesc
+
+  journey.save(function(error, journey) {
     if (error) {
-      return next(error);
+      console.log(error)
+      return next(error)
     }
-    response.json({ message: 'Journey added!', data: journey });
-  });
-});
+    res.json({
+      message: 'Journey added!',
+      data: journey
+    })
+  })
+})
 
 app.post('/delete_journey', (request, response, next) => {
   Journeys.findByIdAndRemove(request.body._id, (error, journey) => {
-    if (error) response.send(error);
-    response.json({ message: 'Journey deleted!', data: journey });
-  });
-});
+    if (error) response.send(error)
+    response.json({ message: 'Journey deleted!', data: journey })
+  })
+})
 
 // LISTENING
-const port = 8086;
-app.listen(port);
-console.log('App listening on port ' + port);
+const port = 8086
+app.listen(port)
+console.log('App listening on port ' + port)
