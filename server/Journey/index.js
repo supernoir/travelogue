43e54f73d4507cb6c61db6e06f343349847e54e0
Conny Kawohl <conny.kawohl@useeds.de>
2018-01-03 11:27:35 +0100

    converts Journey class to Typescript

--- server/Journey/index.js
+++ server/Journey/index.js
@@ -1,42 +1,42 @@
-class Journey {
-  constructor(name, desc, dateRange, milestones) {
-    this.journeyName = name;
-    this.journeyDesc = desc;
-    this.journeyDateRange = dateRange;
-    this.journeyMilestones = milestones;
-  }
-  getJourneyName() {
-    return this.journeyName;
-  }
-  getJourneyDesc() {
-    return this.journeyDesc;
-  }
-  getJourneyDateRange() {
-    return this.journeyDateRange;
-  }
-  getJourneyMilestones() {
-    return this.journeyMilestones;
-  }
-
-  addMilestoneToJourney(newMilestone) {
-    let currentSet = this.journeyMilestones;
-    currentSet.push(newMilestone);
-    this.journeyMilestones = currentSet;
-  }
-
-  addStartDate(startDate) {
-    if (!this.journeyDateRange[0]) {
-      return this.journeyDateRange[0].push(startDate);
+"use strict";
+var Journey = /** @class */ (function () {
+    function Journey(name, desc, dateRange, milestones) {
+        this.journeyName = name;
+        this.journeyDesc = desc;
+        this.journeyDateRange = dateRange;
+        this.journeyMilestones = milestones;
     }
-  }
-
-  addEndDate(endDate) {
-    let endKey = this.getJourneyDateRange().length();
-    parseInt(endKey);
-    if (!this.journeyDateRange[endKey]) {
-      return this.journeyDateRange[endKey].push(endDate);
-    }
-  }
-}
-
+    Journey.prototype.getJourneyName = function () {
+        return this.journeyName;
+    };
+    Journey.prototype.getJourneyDesc = function () {
+        return this.journeyDesc;
+    };
+    Journey.prototype.getJourneyDateRange = function () {
+        return this.journeyDateRange;
+    };
+    Journey.prototype.getJourneyMilestones = function () {
+        return this.journeyMilestones;
+    };
+    Journey.prototype.addMilestoneToJourney = function (newMilestone) {
+        var currentSet = this.journeyMilestones;
+        currentSet.push(newMilestone);
+        this.journeyMilestones = currentSet;
+    };
+    Journey.prototype.addStartDate = function (startDate) {
+        if (!this.journeyDateRange[0]) {
+            return this.journeyDateRange[0] = startDate;
+        }
+    };
+    Journey.prototype.addEndDate = function (endDate) {
+        var endKey = this.getJourneyDateRange().length;
+        var parsedEndKey = endKey;
+        console.log(typeof parsedEndKey);
+        //parseInt(endKey);
+        if (!this.journeyDateRange[endKey]) {
+            return this.journeyDateRange[endKey] = endDate;
+        }
+    };
+    return Journey;
+}());
 module.exports = Journey;
---
+++ server/Journey/index.ts
@@ -0,0 +1,49 @@
+class Journey {
+  journeyName: String
+  journeyDesc: String
+  journeyDateRange: Array<String>
+  journeyMilestones: Array<Object>
+
+  constructor(name: String, desc: String, dateRange: Array<String>, milestones: Array<Object>) {
+    this.journeyName = name;
+    this.journeyDesc = desc;
+    this.journeyDateRange = dateRange;
+    this.journeyMilestones = milestones;
+  }
+  getJourneyName() {
+    return this.journeyName;
+  }
+  getJourneyDesc() {
+    return this.journeyDesc;
+  }
+  getJourneyDateRange() {
+    return this.journeyDateRange;
+  }
+  getJourneyMilestones() {
+    return this.journeyMilestones;
+  }
+
+  addMilestoneToJourney(newMilestone: Object) {
+    let currentSet = this.journeyMilestones;
+    currentSet.push(newMilestone);
+    this.journeyMilestones = currentSet;
+  }
+
+  addStartDate(startDate: string) {
+    if (!this.journeyDateRange[0]) {
+      return this.journeyDateRange[0] = startDate;
+    }
+  }
+
+  addEndDate(endDate: string) {
+    let endKey = this.getJourneyDateRange().length;
+    let parsedEndKey = endKey as number
+    console.log(typeof parsedEndKey)
+    //parseInt(endKey);
+    if (!this.journeyDateRange[endKey]) {
+      return this.journeyDateRange[endKey] = endDate;
+    }
+  }
+}
+
+export = Journey;
