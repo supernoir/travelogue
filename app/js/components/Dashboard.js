1d8155c498108c6ef5d9e8a927fe1413359cf617
supernoir <kawohl.bentodesign@gmail.com>
2018-01-10 00:35:57 +0100

    plays with css grids

--- app/js/components/Dashboard.js
+++ app/js/components/Dashboard.js
@@ -7,6 +7,13 @@ export default class Dashboard extends React.Component {
       <div className="container">
         <Header />
         <h1>Dashboard</h1>
+        <div className="wrapper">
+          <div className="header">HEADER</div>
+          <div className="nav">MENU</div>
+          <div className="content">CONTENT</div>
+          <div className="footer">FOOTER</div>
+        </div>
+
       </div>
     )
   }
