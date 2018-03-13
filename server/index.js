b99421e2b4e9cee350b918e6e1bd5a3c7bc5d2ca
supernoir <kawohl.bentodesign@gmail.com>
2018-03-14 00:40:26 +0100

    refactors server

--- server/index.js
+++ server/index.js
@@ -19,174 +19,189 @@ const logDirectory = path.join(__dirname, 'log');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 const accessLogStream = rfs('access.log', {
-  interval: '1d',
-  path: logDirectory
+	interval: '1d',
+	path: logDirectory
 });
 
 app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+	bodyParser.urlencoded({
+		extended: true
+	})
 );
 
 app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*');
-  response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
-  next();
+	response.header('Access-Control-Allow-Origin', '*');
+	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	response.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
+	next();
+});
+
+const Users = mongoose.model('Users', {
+	id: Number,
+	firstName: String,
+	lastName: String,
+	fullName: String,
+	roles: Array,
+	langs: Array
 });
 
 const Characters = mongoose.model('Characters', {
-  text: String
+	text: String
 });
 
 const Journeys = mongoose.model('Journeys', {
-  name: String,
-  desc: String,
-  cast: Array,
-  startdate: String,
-  enddate: String,
-  daterange: Array,
-  milestones: Array
+	name: String,
+	desc: String,
+	cast: Array,
+	startdate: String,
+	enddate: String,
+	daterange: Array,
+	milestones: Array
 });
 
 const Milestones = mongoose.model('Journey', {
-  journey: String,
-  name: String,
-  date: String,
-  desc: String,
-  loc: String,
-  cast: Array
+	journey: String,
+	name: String,
+	date: String,
+	desc: String,
+	loc: String,
+	cast: Array
 });
 
 /** REST: Base Route */
 const baseRoute = () => {
-  app.get('/', async (req, res) => {
-    await res.json({
-      message: 'Please use a descriptive route. See the API documentation for reference'
-    });
-  });
+	app.get('/', (req, res) => {
+		res.json({
+			message: 'Please use a descriptive route. See the API documentation for reference'
+		});
+	});
 };
-
 baseRoute();
+
+/** REST: Users Route */
+const usersRoute = () => {
+	app.get('/users', (req, res) => {
+		Users.find((error, users) => {
+			if (error) res.send(error);
+			res.json(users);
+		});
+	});
+};
+usersRoute();
 
 /** REST: Characters Route */
 const characterRoute = () => {
-  app.get('/characters', async (req, res) => {
-    await Characters.find((error, characters) => {
-      if (error) res.send(error);
-      res.json(characters);
-    });
-  });
+	app.get('/characters', (req, res) => {
+		Characters.find((error, characters) => {
+			if (error) res.send(error);
+			res.json(characters);
+		});
+	});
 };
 characterRoute();
 
 /** REST: Journeys - GET all Journeys  */
 const journeysRoute = () => {
-  app.get('/journeys/all', async (request, response) => {
-    await Journeys.find((error, journeys) => {
-      if (error) response.send(error);
-      response.json(journeys);
-    });
-  });
+	app.get('/journeys/all', (request, response) => {
+		Journeys.find((error, journeys) => {
+			if (error) response.send(error);
+			response.json(journeys);
+		});
+	});
 };
 journeysRoute();
 
 /** REST: Journeys - GET Single Journey by Name */
-app.get('/journeys/:name', async (req, res) => {
-  await Journeys.findOne({ name: req.params.name }, function(err, journey) {
-    res.json({ journey });
-  });
+app.get('/journeys/:name', (req, res) => {
+	Journeys.findOne({ name: req.params.name }, function(err, journey) {
+		res.json({ journey });
+	});
 });
 
 /** REST: Journeys - POST new Journey */
-app.post('/journeys', async (req, res) => {
-  const journeyName = req.param('name');
-  const journeyCast = req.param('cast');
-  const journeyDesc = req.param('desc');
-  const journeyDateRange = req.param('daterange');
-  const journeyStartDate = req.param('startdate');
-  const journeyEndDate = req.param('enddate');
+app.post('/journeys', (req, res) => {
+	const journeyName = req.param('name');
+	const journeyCast = req.param('cast');
+	const journeyDesc = req.param('desc');
+	const journeyDateRange = req.param('daterange');
+	const journeyStartDate = req.param('startdate');
+	const journeyEndDate = req.param('enddate');
 
-  const journey = new Journeys();
-  journey.name = journeyName;
-  journey.cast = journeyCast;
-  journey.desc = journeyDesc;
-  journey.daterange = journeyDateRange;
-  journey.startdate = journeyStartDate;
-  journey.enddate = journeyEndDate;
+	const journey = new Journeys();
+	journey.name = journeyName;
+	journey.cast = journeyCast;
+	journey.desc = journeyDesc;
+	journey.daterange = journeyDateRange;
+	journey.startdate = journeyStartDate;
+	journey.enddate = journeyEndDate;
 
-  await journey.save((error, journey) => {
-    if (error) {
-      console.log(error);
-      return next(error);
-    }
-    res.json({
-      message: 'Journey added!',
-      data: journey
-    });
-  });
+	journey.save((error, journey) => {
+		if (error) {
+			throw error;
+		}
+		res.json({
+			message: 'Journey added!',
+			data: journey
+		});
+	});
 });
 
 /** REST: Journeys - DELETE Journey */
-app.post('/delete_journey', async (request, response, next) => {
-  await Journeys.findByIdAndRemove(request.body._id, (error, journey) => {
-    if (error) response.send(error);
-    response.json({ message: 'Journey deleted!', data: journey });
-  });
+app.post('/delete_journey', (request, response, next) => {
+	Journeys.findByIdAndRemove(request.body._id, (error, journey) => {
+		if (error) response.send(error);
+		response.json({ message: 'Journey deleted!', data: journey });
+	});
 });
 
 /** REST: Milestones  Route */
 const milestonesRoute = () => {
-  app.get('/milestones/all', async (request, response) => {
-    await Milestones.find((error, milestones) => {
-      if (error) response.send(error);
-      response.json(milestones);
-    });
-  });
+	app.get('/milestones/all', (request, response) => {
+		Milestones.find((error, milestones) => {
+			if (error) response.send(error);
+			response.json(milestones);
+		});
+	});
 };
 milestonesRoute();
 
-app.get('/milestones/:name', async (req, res) => {
-  await Milestones.findOne({ name: req.params.name }, function(err, milestone) {
-    res.json({ milestone });
-  });
+app.get('/milestones/:name', (req, res) => {
+	Milestones.findOne({ name: req.params.name }, function(err, milestone) {
+		res.json({ milestone });
+	});
 });
 
-app.post('/milestones/', async (req, res) => {
-  const milestoneName = req.param('name');
-  const milestoneJourney = req.param('journey');
-  const milestoneDate = req.param('date');
-  const milestoneDesc = req.param('desc');
-  const milestoneCast = req.param('cast');
-  const milestoneLoc = req.param('loc');
+app.post('/milestones/', (req, res) => {
+	const milestoneName = req.param('name');
+	const milestoneJourney = req.param('journey');
+	const milestoneDate = req.param('date');
+	const milestoneDesc = req.param('desc');
+	const milestoneCast = req.param('cast');
+	const milestoneLoc = req.param('loc');
 
-  const milestone = new Milestones();
-  milestone.name = milestoneName;
-  milestone.journey = milestoneJourney;
-  milestone.date = milestoneDate;
-  milestone.desc = milestoneDesc;
-  milestone.cast = milestoneCast;
-  milestone.loc = milestoneLoc;
+	const milestone = new Milestones();
+	milestone.name = milestoneName;
+	milestone.journey = milestoneJourney;
+	milestone.date = milestoneDate;
+	milestone.desc = milestoneDesc;
+	milestone.cast = milestoneCast;
+	milestone.loc = milestoneLoc;
 
-  await milestone.save((err, milestone) => {
-    if (err) {
-      return next(err);
-    }
-    res.json({ message: 'Milestone added', data: milestone });
-  });
+	milestone.save((err, milestone) => {
+		if (err) {
+			throw err;
+		}
+		res.json({ message: 'Milestone added', data: milestone });
+	});
 });
 
 // CERTS
 const options = {
-  key: fs.readFileSync(__dirname + '/cert/server.key'),
-  cert: fs.readFileSync(__dirname + '/cert/server.crt')
+	key: fs.readFileSync(__dirname + '/cert/server.key'),
+	cert: fs.readFileSync(__dirname + '/cert/server.crt')
 };
 
 // LISTENING
@@ -194,11 +209,6 @@ const port = 8086 || process.env.port;
 /* app.listen(port);
 console.log('App listening on port ' + port); */
 
-spdy.createServer(options, app).listen(port, error => {
-  if (error) {
-    console.error(error);
-    return process.exit(1);
-  } else {
-    console.log('Listening on port: ' + port + '.');
-  }
-});
+const runServer = spdy.createServer(options, app);
+runServer.listen(port);
+console.log(`Listening on port ${port}`);
